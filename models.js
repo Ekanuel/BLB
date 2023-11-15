769839145cb8459fe5394420a644066a9ed564b1
Ekanuel <ekanumaf@gmail.com>
2023-11-15 15:58:45 +0300

    add all

--- models.js
+++ models.js
@@ -1,14 +1,39 @@
 //Remember to import in mongoose
+const mongoose = require('mongoose');
 
 
 //create the citizen schema here with the appropriate data types
+const citizenSchema = new mongoose.Schema({
+    name: String,
+    dateOfBirth: Date,
+    fatherName: String,
+    motherName: String,
+    gender: String,
+    bloodGroup: String,
+});
 
 
-//create the title schema here wit the right fields
+//create the title schema here with the right fields
+const titleSchema = new mongoose.Schema({
+    ownerName: String,
+    location: String,
+    size: String,
+    coordinates: String,
+    titleNumber: {
+        type: String,
+        unique: true
+    },
+    satellitePhoto: String,
+});
 
 
-//convert the schemas into models from here
 
+// Convert the schemas into models
+const Citizen = mongoose.model('Citizen', citizenSchema);
+const Title = mongoose.model('Title', titleSchema);
 
-//export the models as modules from here.
-
+// Export the models as modules
+module.exports = {
+    Citizen,
+    Title
+};
--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 //this is where your express sever is created and managed
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //do not forget to import the necessary modules,
+const tables = require('./models');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //do not forget to connect to your mongoDb
-
-
+mongoose.connect('mongodb+srv://your-connection-string', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //do not forget to set up routes to serve out the html file
+app.post('/citizen', async (req, res) => {
+    try {
+        const newCitizen = await Citizen.create(req.body);
+        res.status(201).json(newCitizen);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/title', async (req, res) => {
+    try {
+        const newTitle = await Title.create(req.body);
+        res.status(201).json(newTitle);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/citizens', async (req, res) => {
+    try {
+        const citizens = await Citizen.find();
+        res.status(200).json(citizens);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/titles', async (req, res) => {
+    try {
+        const titles = await Title.find();
+        res.status(200).json(titles);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 // Do not forget to handle your form posts for the relevant forms
 
@@ -17,3 +60,6 @@
 //Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
 
 // do not forget to create a port for your application and serving out the appliacation via the port created 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
